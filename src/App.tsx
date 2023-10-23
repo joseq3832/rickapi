206b697e6546392fe7d3d0d8c7c32a7a85029dac
José Alarcón <joseq.ja@gmail.com>
2023-10-23 18:07:46 -0500

    feat: :tada: Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { RouterProvider } from 'react-router-dom'
 import './App.css'
+import { routes } from './routes'
 
-function App() {
+const queryClient = new QueryClient()
+
+const App = (): JSX.Element => {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={routes} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   )
 }
 
